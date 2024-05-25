516a90f886521215df9caa2a7726d2df4515a2df
GagoPalyan <134396565+GagoPalyan@users.noreply.github.com>
2024-05-25 19:54:00 +0400

    AboutUs page media is ready

--- src/components/AboutUsPage/ImgContent/ImgContent.jsx
+++ src/components/AboutUsPage/ImgContent/ImgContent.jsx
@@ -1,3 +1,4 @@
+import Media from "react-media";
 import Title from "../../../sharedComponents/Title/Title";
 import style from "./ImgContent.module.css";
 
@@ -8,22 +9,36 @@ function ImgContent({
   ...props
 }) {
   return (
-    <section
-      style={reverse ? { flexDirection: "row-reverse" } : null}
-      className={style.content}
-    >
-      <img src={props.image} />
-      <div className={style.textContent}>
-        <div
-          className={
-            titlePosition == "left" ? style.titleLeft : style.titleCenter
-          }
-        >
-          <Title caption={props.caption} text={props.text} />
-        </div>
-        {children}
-      </div>
-    </section>
+    <Media query={"(max-width: 1860px)"}>
+      {(matchers) => {
+        return !matchers ? (
+          <section
+            style={reverse ? { flexDirection: "row-reverse" } : null}
+            className={style.content}
+          >
+            <img src={props.image} />
+            <div className={style.textContent}>
+              <div
+                className={
+                  titlePosition == "left" ? style.titleLeft : style.titleCenter
+                }
+              >
+                <Title caption={props.caption} text={props.text} />
+              </div>
+              {children}
+            </div>
+          </section>
+        ) : (
+          <section
+            style={{ backgroundImage: `url("${props.image}")` }}
+            className={style.background}
+          >
+            <Title caption={props.caption} text={props.text} />
+            {children}
+          </section>
+        );
+      }}
+    </Media>
   );
 }
 
